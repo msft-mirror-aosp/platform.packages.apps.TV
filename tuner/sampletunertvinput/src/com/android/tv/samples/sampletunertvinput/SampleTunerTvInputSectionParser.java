792fde7b15cf01eaae2e95fad051d0561355a033
Lucas Gates <lucasgates@google.com>
2022-06-29 01:55:09 +0000

    SampleTunerTIS SectionParser add PSIP check
    This CL adds functionality to our SectionParser which
    checks whether the incoming data is a valid PSIP section,
    including validating its CRC32/MPEG-2 sum.
    
    Bug: 237323181
    Test: Manually this Cuttlefish. Should see invalid sections
    or section without correct CRC32 rejected.
    
    Change-Id: I151404a23baacbf47e28f7de0eac4ea6d079deb2

--- tuner/sampletunertvinput/src/com/android/tv/samples/sampletunertvinput/SampleTunerTvInputSectionParser.java
+++ tuner/sampletunertvinput/src/com/android/tv/samples/sampletunertvinput/SampleTunerTvInputSectionParser.java
@@ -22,6 +22,7 @@ import java.util.Locale;
 
 /** Parser for ATSC PSIP sections */
 public class SampleTunerTvInputSectionParser {
+    private static final String TAG = "SampleTunerTvInput";
 
     /**
      * Parses a single TVCT section, as defined in A/65 6.4
@@ -29,8 +30,36 @@ public class SampleTunerTvInputSectionParser {
      * @return null if there is an error while parsing, the channel with parsed data otherwise
      */
     public static TvctChannelInfo parseTvctSection(byte[] data) {
+        if (!checkValidPsipSection(data)) {
+            return null;
+        }
         // TODO: Parse the data for channel information
         return new TvctChannelInfo("Sample Channel", 1, 1);
+    }
+
+    private static boolean checkValidPsipSection(byte[] data) {
+        if (data.length < 13) {
+            Log.e(TAG, "Section was too small");
+            return false;
+        }
+        if ((data[0] & 0xff) == 0xff) {
+            // Should clear stuffing bytes as detailed by H222.0 section 2.4.4.
+            Log.e(TAG, "Unexpected stuffing bytes while parsing section");
+            return false;
+        }
+        int sectionLength = (((data[1] & 0x0f) << 8) | (data[2] & 0xff)) + 3;
+        if (sectionLength != data.length) {
+            Log.e(TAG, "Length mismatch while parsing section");
+            return false;
+        }
+        int sectionNumber = data[6] & 0xff;
+        int lastSectionNumber = data[7] & 0xff;
+        if(sectionNumber > lastSectionNumber) {
+            Log.e(TAG, "Found sectionNumber > lastSectionNumber while parsing section");
+            return false;
+        }
+        // TODO: Check CRC 32/MPEG for validity
+        return true;
     }
 
     // Contains the portion of the data contained in the TVCT used by
