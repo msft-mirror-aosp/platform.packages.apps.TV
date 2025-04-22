e9e39fa4f1f03742ab8257193c0bfdcadf7ca3f9
Kevin Liu <congxiliu@google.com>
2025-04-22 19:48:55 +0000

    Replaced the deprecated fileFromPath() method
    As the deprecated method fileFromPath was removed. Directly using
    the underlying method Fs.fromUrl().
    
    Flag: EXEMPT Roboletric deprecated method change
    Bug: 412352186
    Test: atest TvTunerRoboTests
    Change-Id: If21d0633a0bd509ed7916d363b9b56c8a80045a1

--- tuner/tests/robotests/javatests/com/android/tv/tuner/testing/TvTunerRobolectricTestRunner.java
+++ tuner/tests/robotests/javatests/com/android/tv/tuner/testing/TvTunerRobolectricTestRunner.java
@@ -50,9 +50,9 @@ public class TvTunerRobolectricTestRunner extends RobolectricTestRunner {
         // By adding any resources from libraries we need the AndroidManifest, we can access
         // them from within the parallel universe's resource loader.
         return new AndroidManifest(
-                Fs.fileFromPath(config.manifest()),
-                Fs.fileFromPath(config.resourceDir()),
-                Fs.fileFromPath(config.assetDir()),
+                Fs.fromUrl(config.manifest()),
+                Fs.fromUrl(config.resourceDir()),
+                Fs.fromUrl(config.assetDir()),
                 packageName) {
             @Override
             public List<ResourcePath> getIncludedResourcePaths() {
@@ -64,6 +64,6 @@ public class TvTunerRobolectricTestRunner extends RobolectricTestRunner {
     }
 
     public static void getIncludedResourcePaths(List<ResourcePath> paths) {
-        paths.add(new ResourcePath(null, Fs.fileFromPath("./packages/apps/TV/tuner/res"), null));
+        paths.add(new ResourcePath(null, Fs.fromUrl("./packages/apps/TV/tuner/res"), null));
     }
 }
