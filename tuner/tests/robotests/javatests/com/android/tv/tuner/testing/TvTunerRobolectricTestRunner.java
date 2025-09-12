04938856dcecdc58f3c93fa5689f7bedf4cc5909
congxiliu <congxiliu@google.com>
2025-09-12 17:26:57 +0000

    Removed getAppManifest method
    As config.resourceDir() and config.assetDir() has been removed from
    library. The legacy way of programmatically constructing an
    AndroidManifest should be updated to Android.bp in Soong
    In addition, there seems to be no usage of this method in git_main.
    
    This is necessary as the latest update from Robolectric library has removed this deprecated method. The update ag/35465139 is currently blocked by this update.
    
    Test: atest TvRoboTests
    Bug: 443072871
    
    Flag: EXEMPT TEST_ONLY
    
    Change-Id: Idc185f426deefc43c1f1c69a361c3838f0523d86

--- tuner/tests/robotests/javatests/com/android/tv/tuner/testing/TvTunerRobolectricTestRunner.java
+++ tuner/tests/robotests/javatests/com/android/tv/tuner/testing/TvTunerRobolectricTestRunner.java
@@ -18,8 +18,6 @@ package com.android.tv.tuner.testing;
 
 import org.junit.runners.model.InitializationError;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-import org.robolectric.manifest.AndroidManifest;
 import org.robolectric.res.Fs;
 import org.robolectric.res.ResourcePath;
 
@@ -38,29 +36,6 @@ public class TvTunerRobolectricTestRunner extends RobolectricTestRunner {
     /** We don't actually want to change this behavior, so we just call super. */
     public TvTunerRobolectricTestRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
-    }
-
-    /**
-     * We are going to create our own custom manifest so that we can add multiple resource paths to
-     * it.
-     */
-    protected AndroidManifest getAppManifest(Config config) {
-        final String packageName = "com.android.tv.tuner";
-
-        // By adding any resources from libraries we need the AndroidManifest, we can access
-        // them from within the parallel universe's resource loader.
-        return new AndroidManifest(
-                Fs.fromUrl(config.manifest()),
-                Fs.fromUrl(config.resourceDir()),
-                Fs.fromUrl(config.assetDir()),
-                packageName) {
-            @Override
-            public List<ResourcePath> getIncludedResourcePaths() {
-                List<ResourcePath> paths = super.getIncludedResourcePaths();
-                TvTunerRobolectricTestRunner.getIncludedResourcePaths(paths);
-                return paths;
-            }
-        };
     }
 
     public static void getIncludedResourcePaths(List<ResourcePath> paths) {
